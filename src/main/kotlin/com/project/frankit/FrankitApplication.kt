7c631ef45457ad470e58a14355a1e6ff5bd0ca2b
DonghwanSon1 <sdh549000@gmail.com>
2025-02-17 18:26:50 +0900

    git init

---
+++ src/main/kotlin/com/project/frankit/FrankitApplication.kt
@@ -0,0 +1,11 @@
+package com.project.frankit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrankitApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrankitApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/frankit/FrankitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.frankit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrankitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
