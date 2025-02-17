ae3f9729ff74b119ac2695d66686374c26e12337
DonghwanSon1 <sdh549000@gmail.com>
2025-02-17 21:32:29 +0900

    style : flyway 설명 수정

--- src/main/resources/db/migration/V1.0__create_member.sql
+++ src/main/resources/db/migration/V1.0__create_member.sql
@@ -1,10 +1,10 @@
--- 유저 테이블 (users)
+-- 회원 테이블 (Member)
 CREATE TABLE IF NOT EXISTS member (
-    `sn` BIGINT AUTO_INCREMENT PRIMARY KEY,        -- 유저 SN
-    `email` VARCHAR(30) NOT NULL,                  -- 사용자 ID (고유)
+    `sn` BIGINT AUTO_INCREMENT PRIMARY KEY,        -- 회원 SN
+    `email` VARCHAR(30) NOT NULL,                  -- 사용자 Email (고유)
     `password` VARCHAR(100) NOT NULL,              -- 비밀번호
     `name` VARCHAR(10) NOT NULL,                   -- 이름
     `store_name` VARCHAR(100) NOT NULL,            -- 가게 이름
     `role` VARCHAR(30) NOT NULL,                   -- 역할
-    UNIQUE KEY unique_id (`email`)                 -- 유저 테이블 유니크 키 관계
+    UNIQUE KEY unique_id (`email`)                 -- 회원 테이블 유니크 키 관계
 );
